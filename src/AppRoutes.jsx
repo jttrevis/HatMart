353188a1c04281100faef0a16e6d4c98386d1560
jttrevis <jr.trevis@hotmail.com>
2023-03-21 08:59:27 +0000

    .

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -10,7 +10,14 @@ const AppRoutes = () => {
 	useLayoutEffect(() => {
 		window.scrollTo(0, 0);
 	}, [location.pathname]);
-	return <h1></h1>;
+	return (
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/shop" element={<Shop />} />
+			<Route path="/hat/:id?" element={<ProductPage />} />
+			<Route path="/about" element={<About />} />
+		</Routes>
+	);
 };
 
 export default AppRoutes;
--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,9 @@ import { client } from './services/prismic';
 import { CartProvider } from './context/CartContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<CartProvider>
-			<PrismicProvider client={client}>
-				<App />
-			</PrismicProvider>
-		</CartProvider>
-	</React.StrictMode>
+	<CartProvider>
+		<PrismicProvider client={client}>
+			<App />
+		</PrismicProvider>
+	</CartProvider>
 );
